82610abd8bec21baaa9d8a9f471cf63f6e442487
Niaz Ul Haque <theniaz619.2@gmail.com>
2020-04-30 02:46:50 -0400

    Updates

---
+++ precache-manifest.f2f483969c87c73bde4f3143564ad727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea6f22b358e29743b2f04e397a4ea61",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "94c930791d73fc881581",
+    "url": "/covidtracker/static/css/main.808148a6.chunk.css"
+  },
+  {
+    "revision": "b02380d2294ea2b31a52",
+    "url": "/covidtracker/static/js/2.8f7b7032.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.8f7b7032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c930791d73fc881581",
+    "url": "/covidtracker/static/js/main.567ba6ad.chunk.js"
+  },
+  {
+    "revision": "40dbf722d8a79e05ca17",
+    "url": "/covidtracker/static/js/runtime-main.30f1e191.js"
+  },
+  {
+    "revision": "2072d359d85d8683a03a5065d4ce2d18",
+    "url": "/covidtracker/static/media/image.2072d359.png"
+  }
+]);
